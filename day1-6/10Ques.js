9208cf00de3419edefb7299e0dabc45372bb05e8
monishjodha <monish.21jics202@jietjodhpur.ac.in>
2023-06-12 20:17:39 +0530

    first commit

---
+++ day1-6/10Ques.js
@@ -0,0 +1,101 @@
+// x=[];
+// for (i=1;i<=5;i++){
+//     for (j=1;j<=i;j++){
+//         x=x+"*";
+        
+//     }x=x+"\n";
+
+// }
+// console.log(x)
+
+// x=[];
+// for (i=0;i<=2;i++){
+//     for (j=1;j<=2*i+1;j++){
+//         x=x+"*";
+        
+//     }x=x+"\n";
+
+// }
+
+// for (i=2;i>=1;i--){
+//     for (j=1;j<=2*i-1;j++){
+//         x=x+"*";
+        
+//     }x=x+"\n";
+
+// }
+
+// console.log(x)
+
+
+
+// x=[];
+// for (i=1;i<=3;i++){
+//     for (j=1;j<=i;j++){
+//         x=x+"*";
+        
+//     }x=x+"\n";
+
+// }
+// for (i=2;i>=1;i--){
+//     for (j=1;j<=i;j++){
+//         x=x+"*";
+        
+//     }x=x+"\n";
+
+// }
+
+// console.log(x)
+
+// x=[1,3,2,4,5,6,6,7,8,9]
+// a=0
+// for (i=0;i<10;i++){
+//     a=(a + x[i]);
+// }
+// console.log(a);
+// console.log(a/10);
+
+// var m=3;
+// var m1;
+// switch (m) {
+//     case 1: m1="Jan" 
+//         break;
+//     case 2: m1="Feb" 
+//         break;
+//     case 3: m1="Mar" 
+//         break;
+//     case 4: m1="Apr" 
+//         break;
+//     case 5: m1="May" 
+//         break;
+
+//     case 6: m1="Jun" 
+//         break;
+//     case 7: m1="Jul" 
+//         break;
+//      case 8: m1="Aug" 
+//         break;
+//     case 9: m1="sep" 
+//         break;
+//     case 10: m1="Oct" 
+//         break;
+//     case 11: m1="Nov" 
+//         break;
+    
+//     default: m1="Dec" 
+//         break;
+// }
+
+// console.log(m1);
+
+// x=[1,3,2,4,5,6,6,7,8,9];
+// const y= x.sort(function(a, b){return a - b});
+// console.log(y);
+// function table (a){
+//     for(i=1;i<=10;i++){
+//         console.log(a+" X "+i+" = "+a*i);
+//     }
+// }
+// for (j=2;j<=10;j++){
+//     console.log(table(j));
+// }
