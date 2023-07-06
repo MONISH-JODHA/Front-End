57a757b4207b38888c3ae794fe90a6aa1a8eebaf
monishjodha <monish.21jics202@jietjodhpur.ac.in>
2023-07-06 14:53:36 +0530

    call bind and apply created

--- day-7/prac.js
+++ day-7/prac.js
@@ -1,11 +1,33 @@
-function add(one,two){
-    this.one=one
-    this.two=two
-}
-let mj = new add("try","light");
+// function add(one,two){
+//     this.one=one
+//     this.two=two
+// }
+// let mj = new add("try","light");
 
-add.prototype.make= function(){
-    return this.one+ " "+ this.two
+// add.prototype.make= function(){
+//     return this.one+ " "+ this.two
+// }
+
+// console.log(mj.make());
+
+let monish ={
+    class:'fourth',
+    surname:"Jodha",
+    printname: function(){
+        console.log( this.class)
+    }
 }
 
-console.log(mj.make());
+function mj(x,y){
+    
+        console.log(this.surname + this.printname())
+        console.log(x+y);
+    }
+
+
+// let jodha=mj.bind(monish,1,2)
+// console.log(jodha());
+
+// let jodha1=mj.call(monish,1,1);
+
+let jodha2=mj.apply(monish,[5,5]);
